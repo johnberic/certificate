7451d4e6f210b55c9e09d25433a3b54e301dce10
tknetwork <154647403+johnberic@users.noreply.github.com>
2026-09-11 11:51:56 +0800

    Modify go_disconnect.sh for user status update
    Updated the script to check if USERNAME is set before executing database updates. Changed server identifier in the online count update.

--- openconnect/go_disconnect.sh
+++ openconnect/go_disconnect.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-source ~/.db-base
-MYSQL_PWD=$DB_PASS mysql -u $DB_USER -h $DB_HOST -D $DB_NAME  -sse "UPDATE users SET is_connected='0', active_address='', active_date='' WHERE user_name='$USERNAME'"
-MYSQL_PWD=$DB_PASS mysql -u $DB_USER -h $DB_HOST -D $DB_NAME  -sse "UPDATE server_list SET online=online-1 WHERE server_ip='1'"
+source /root/.db-base
+
+# Siguraduhing may laman ang USERNAME na pinasa ng ocserv
+if [ -z "$USERNAME" ]; then
+    exit 0
+fi
+
+# 1. I-update ang status ng user sa database bilang disconnected
+MYSQL_PWD=$DB_PASS mysql -u $DB_USER -h $DB_HOST -D $DB_NAME -sse "UPDATE users SET is_connected='0', active_address='', active_date='' WHERE user_name='$USERNAME'"
+
+# 2. Bawasan ng 1 ang online count gamit ang tamang server identifier (TK403)
+MYSQL_PWD=$DB_PASS mysql -u $DB_USER -h $DB_HOST -D $DB_NAME -sse "UPDATE server_list SET online=online-1 WHERE server_ip='TK403'"
+
+exit 0
